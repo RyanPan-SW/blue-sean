632a865d8ea8c8d84f38c57cd5ec8cb7a2bc8f06
杨焱康 <704799639@qq.com>
2020-11-18 16:47:24 +0800

    利用dotenv-cli实现多环境打包

--- scripts/uat.js
+++ scripts/uat.js
@@ -1,8 +1,8 @@
 'use strict';
 
 // Do this as the first thing so that any code reading it knows the right env.
-process.env.BABEL_ENV = 'production';
-process.env.NODE_ENV = 'production';
+process.env.BABEL_ENV = 'uat';
+process.env.NODE_ENV = 'uat';
 
 // Makes the script crash on unhandled rejections instead of silently
 // ignoring them. In the future, promise rejections that are not handled will
@@ -134,7 +134,7 @@ checkBrowsers(paths.appPath, isInteractive)
 
 // Create the production build and print the deployment instructions.
 function build(previousFileSizes) {
-  console.log(`Creating an optimized ${process.env.REACT_APP_ENV} build...`);
+  console.log(`Creating an optimized ${process.env.REACT_APP_BASEURL} build...`);
 
   const compiler = webpack(config);
   return new Promise((resolve, reject) => {
