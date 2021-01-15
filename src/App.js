520e6e1b7c1190d3b3a00b2f2bdf2937515fb9b0
潘世文 <panshiwen@panshiwendeMacBook-Pro.local>
2021-01-15 19:42:07 +0800

    注释

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import React from 'react'
-import { Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Switch } from 'react-router-dom'
 import routesConfig from './router'
 import { PrivateRoute, RouteWithSubRoutes } from './router/renderRoutes'
 import './App.css'
 
 const App = () => {
   return (
-    <Switch>
-      {routesConfig.map((route, i) => {
-        if (route.auth) {
-          return (
-            <PrivateRoute key={i} path={route.path}>
-              <route.component />
-            </PrivateRoute>
-          )
-        }
-        return <RouteWithSubRoutes key={i} {...route} />
-      })}
-    </Switch>
+    <Router>
+      <Switch>
+        {routesConfig.map((route, i) => {
+          if (route.auth) {
+            return (
+              <PrivateRoute key={i} path={route.path}>
+                <route.component />
+              </PrivateRoute>
+            )
+          }
+          return <RouteWithSubRoutes key={i} {...route} />
+        })}
+      </Switch>
+    </Router>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import { BrowserRouter as Router } from 'react-router-dom'
 import reportWebVitals from './reportWebVitals'
 import configStore from './store'
 import App from './App'
@@ -13,9 +12,7 @@ const store = configStore()
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router>
-      <App />
-    </Router>
+    <App />
   </Provider>,
   document.getElementById('root'),
 )
